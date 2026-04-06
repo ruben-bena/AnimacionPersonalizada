ef001d699fcdc89e964c2c60fba4b8d2ade5ac76
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2026-04-06 10:21:16 +0200

    using only walkframes

--- core/src/main/java/com/rubenbellido/animacionpersonalizada/Main.java
+++ core/src/main/java/com/rubenbellido/animacionpersonalizada/Main.java
@@ -3,39 +3,92 @@ package com.rubenbellido.animacionpersonalizada;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Main implements ApplicationListener {
 
+    // Constant rows and columns of the sprite sheet
+    private static final int FRAME_COLS = 12, FRAME_ROWS = 1;
+
+    // Objects used
+    Animation<TextureRegion> idleAnimation;
+    Animation<TextureRegion> walkAnimation;
+    Animation<TextureRegion> backAnimation;
+    Texture idleSheet;
+    Texture walkSheet;
+    Texture backSheet;
     SpriteBatch spriteBatch;
-    Player player;
+
+    // A variable for tracking elapsed time for the animation
+    float stateTime;
 
     @Override
     public void create() {
+
+        // Load the sprite sheet as a Texture
+        walkSheet = new Texture(Gdx.files.internal("spritesheet_ruben_fixed.png"));
+
+        // Use the split utility method to create a 2D array of TextureRegions. This is
+        // possible because this sprite sheet contains frames of equal size and they are
+        // all aligned.
+        TextureRegion[][] tmp = TextureRegion.split(walkSheet,
+            walkSheet.getWidth() / FRAME_COLS,
+            walkSheet.getHeight() / FRAME_ROWS);
+
+        // Place the regions into a 1D array in the correct order, starting from the top
+        // left, going across first. The Animation constructor requires a 1D array.
+        TextureRegion[] walkFrames = new TextureRegion[6];
+        int index = 0;
+        for (int i = 0; i < FRAME_ROWS; i++) {
+            for (int j = 0; j < FRAME_COLS; j++) {
+                if (j > 2 && j<9) {
+                    walkFrames[index++] = tmp[i][j];
+                }
+            }
+        }
+
+        // Initialize the Animation with the frame interval and array of frames
+        walkAnimation = new Animation<TextureRegion>(0.1f, walkFrames);
+
+        // Instantiate a SpriteBatch for drawing and reset the elapsed animation
+        // time to 0
         spriteBatch = new SpriteBatch();
-        player = new Player();
+        stateTime = 0f;
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
     }
 
     @Override
     public void render() {
-        float deltaTime = Gdx.graphics.getDeltaTime();
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
+        stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
 
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        player.update(deltaTime);
-
+        // Get current frame of animation for the current stateTime
+        TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
         spriteBatch.begin();
-        player.render(spriteBatch);
+        spriteBatch.draw(currentFrame, 50, 50); // Draw current frame at (50, 50)
         spriteBatch.end();
     }
 
-    @Override public void resize(int width, int height) {}
-    @Override public void pause() {}
-    @Override public void resume() {}
+    @Override
+    public void pause() {
+
+    }
 
     @Override
-    public void dispose() {
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() { // SpriteBatches and Textures must always be disposed
         spriteBatch.dispose();
-        player.dispose();
+        walkSheet.dispose();
     }
 }
